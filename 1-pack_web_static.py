fa6cbf9f29bd48f03f3e9cf42d9624ee3c9edd38
Carrie Ybay <carrieybay@gmail.com>
2017-08-21 20:53:12 -0700

    Initial commit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Fabric script that generates a .tgz archive from web_static folder
+"""
+
+
+from fabric.api import *
+import time
+
+
+def do_pack():
+    """creates .tgz archive from web_static folder"""
+    timestr = time.strftime("%Y%m%d%H%M")
+    try:
+        local("mkdir versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(timestr))
+        return ("versions/web_static_{}.tgz".format(timestr))
+    except:
+        return None
