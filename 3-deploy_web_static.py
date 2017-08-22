55ec3f1ea7851038bd8ab827e5b64b851927373b
Carrie Ybay <carrieybay@gmail.com>
2017-08-22 15:27:54 -0700

    Edit mkdir to create directory every time

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -15,7 +15,7 @@ def do_pack():
     """creates .tgz archive from web_static folder"""
     timestr = time.strftime("%Y%m%d%H%M")
     try:
-        local("mkdir versions")
+        local("mkdir -p versions")
         local("tar -czvf versions/web_static_{}.tgz web_static/"
               .format(timestr))
         return ("versions/web_static_{}.tgz".format(timestr))
