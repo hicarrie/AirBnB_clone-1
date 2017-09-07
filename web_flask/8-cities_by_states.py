808096403e13547bc941ba3ff80de4f395a80d05
Carrie Ybay <carrieybay@gmail.com>
2017-09-07 16:37:04 -0700

    Change code to properly show states and cities

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -19,22 +19,8 @@ def teardown(self):
 @app.route('/cities_by_states', strict_slashes=False)
 def cities_by_states():
     """ displays 8-cities_by_states.html """
-    states_all = storage.all("State").values()
     if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-        states = {}
-        for state in states_all:
-            states['id'] = state.id
-            states['name'] = state.name
-            for city in state.cities:
-                cities = {}
-                if city.state_id == state.id:
-                    cities['id'] = city.id
-                    cities['name'] = city.name
-            if len(cities) > 0:
-                states['cities'] = cities
-        print(states)
-    else:
-        cities = states.cities()
+        states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
